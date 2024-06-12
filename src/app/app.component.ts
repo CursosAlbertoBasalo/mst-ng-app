1d0212c28e319c10d234a1a18db9b9635e4ed470
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-12 12:33:22 +0200

    feat: consumer linked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LocationComponent } from '@mst/ui';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LocationComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <h1>Welcome to {{ title }}!</h1>
+    <mst-ui-location [value]="location"></mst-ui-location>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'mst-ng-app';
+  location = 'New York, NY';
 }
