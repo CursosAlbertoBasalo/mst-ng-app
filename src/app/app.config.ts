ae39f6d8a31c13ac343ef04b73b054c8d61085d3
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-17 13:50:45 +0200

    feat: environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideCrudRepository } from '@mst/srv';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -10,6 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([])),
-    provideCrudRepository('http://localhost:3000/api'),
+    provideCrudRepository(environment.apiUrl),
   ],
 };
