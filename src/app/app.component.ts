e1d3da2b578f3b17c05655671fe5002fc52efd26
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-17 12:59:51 +0200

    feat: ng app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CrudRepository } from '@mst/srv';
 import { LocationComponent } from '@mst/ui';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LocationComponent],
+  imports: [RouterOutlet, LocationComponent, AsyncPipe, JsonPipe],
   template: `
     <h1>Welcome to {{ title }}!</h1>
     <mst-ui-location [value]="location"></mst-ui-location>
+    <pre>{{ activities$ | async | json }}</pre>
     <router-outlet />
   `,
   styles: [],
@@ -16,4 +19,6 @@ import { LocationComponent } from '@mst/ui';
 export class AppComponent {
   title = 'mst-ng-app';
   location = 'New York, NY';
+  #repository = inject(CrudRepository);
+  activities$ = this.#repository.getAll('activities');
 }
